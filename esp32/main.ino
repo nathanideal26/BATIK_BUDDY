0008e707e357884f04d86e491870b130c469d793
nathanideal26 <nathanideal26@gmail.com>
2026-08-30 12:05:09 +0800

    Update main.ino

--- esp32/main.ino
+++ esp32/main.ino
@@ -1,1 +1,65 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    }
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+      // Aktifkan pengiklanan BLE semula sekiranya sambungan terputus
+      pServer->getAdvertising()->start();
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      String rxValue = pCharacteristic->getValue().c_str();
+      if (rxValue.length() > 0) {
+        // Arahan diterima daripada Windows Tablet
+        Serial.print("Arahan diterima: ");
+        Serial.println(rxValue);
+      }
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  // Inisialisasi Bluetooth BLE ESP32
+  BLEDevice::init("BATIK_BUDDY_ESP32");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+
+  pCharacteristic->addDescriptor(new BLE2902());
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  BLEDevice::startAdvertising();
+  
+  Serial.println("BATIK BUDDY ESP32 BLE Sedia disambung...");
+}
+
+void loop() {
+  // Masukkan logik pengesanan sensor / pergerakan robot di sini
+}
